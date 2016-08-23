232194284810b2907f82e18fbff7f9d809861fbb
Joël Gähwiler <joel.gaehwiler@gmail.com>
2016-08-23 09:27:01 +0200

    update json keys

--- models.go
+++ models.go
@@ -7,7 +7,7 @@ type Set struct {
 	ID           string        `json:"id"`
 	Title        string        `json:"title"`
 	CreatedAt    time.Time     `json:"created_at"`
-	MediaEntries []*MediaEntry `json:"media"`
+	MediaEntries []*MediaEntry `json:"media_entries"`
 }
 
 // A MediaEntry contains multiple previews.
@@ -18,7 +18,7 @@ type MediaEntry struct {
 	FileID      string     `json:"file_id"`
 	FileName    string     `json:"file_name"`
 	StreamURL   string     `json:"stream_url"`
-	DownloadURL string     `json:"file_url"`
+	DownloadURL string     `json:"download_url"`
 	Previews    []*Preview `json:"previews"`
 }
 
