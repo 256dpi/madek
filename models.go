e38a57175d56ac08214439b88d6bdf472af94ade
Joël Gähwiler <joel.gaehwiler@gmail.com>
2016-08-25 11:19:42 +0200

    fmt

--- models.go
+++ models.go
@@ -7,21 +7,21 @@ type MetaData map[string]string
 
 // A Collection contains multiple media entries.
 type Collection struct {
-	ID           string       `json:"id"`
-	CreatedAt    time.Time    `json:"created_at"`
-	MetaData     MetaData     `json:"meta_data"`
+	ID           string        `json:"id"`
+	CreatedAt    time.Time     `json:"created_at"`
+	MetaData     MetaData      `json:"meta_data"`
 	MediaEntries []*MediaEntry `json:"media_entries"`
 }
 
 // A MediaEntry contains multiple previews.
 type MediaEntry struct {
-	ID          string    `json:"id"`
-	MetaData    MetaData  `json:"meta_data"`
-	CreatedAt   time.Time `json:"created_at"`
-	FileID      string    `json:"file_id"`
-	FileName    string    `json:"file_name"`
-	StreamURL   string    `json:"stream_url"`
-	DownloadURL string    `json:"download_url"`
+	ID          string     `json:"id"`
+	MetaData    MetaData   `json:"meta_data"`
+	CreatedAt   time.Time  `json:"created_at"`
+	FileID      string     `json:"file_id"`
+	FileName    string     `json:"file_name"`
+	StreamURL   string     `json:"stream_url"`
+	DownloadURL string     `json:"download_url"`
 	Previews    []*Preview `json:"previews"`
 }
 
