6768b7d4ca8339c4da2cde15d8d45b27ef677b04
Joël Gähwiler <joel.gaehwiler@gmail.com>
2020-06-16 10:01:44 +0200

    simplified cli

--- cmd/madek/main.go
+++ cmd/madek/main.go
@@ -2,99 +2,39 @@ package main
 
 import (
 	"encoding/json"
+	"flag"
 	"fmt"
-	"net/http"
-	"strings"
-
-	"github.com/patrickmn/go-cache"
 
 	"github.com/IAD-ZHDK/madek"
 )
 
+var address = flag.String("address", "https://medienarchiv.zhdk.ch", "The address of the Madek instance.")
+var username = flag.String("username", "", "The username for authentication.")
+var password = flag.String("password", "", "The password for authentication.")
+
 func main() {
-	cmd := parseCommand()
+	// parse flags
+	flag.Parse()
 
-	client := madek.NewClient(cmd.oAddress, cmd.oUsername, cmd.oPassword)
+	// get id
+	id := flag.Arg(0)
 
-	if cmd.cFetch {
-		fetch(client, cmd.aID)
-	} else if cmd.cServer {
-		server(client, cmd.oCache)
-	}
-}
+	// prepare client
+	client := madek.NewClient(*address, *username, *password)
 
-func fetch(client *madek.Client, id string) {
+	// compile collection
 	coll, err := client.CompileCollection(id)
 	if err != nil {
 		fmt.Printf("Error encountered: %s\n", err)
 		return
 	}
 
+	// encode
 	bytes, err := json.MarshalIndent(coll, "", "  ")
 	if err != nil {
 		panic(err)
 	}
 
+	// print
 	fmt.Println(string(bytes))
-}
-
-func server(client *madek.Client, cacheEnabled bool) {
-	mux := http.NewServeMux()
-
-	requestCache := cache.New(cache.NoExpiration, cache.NoExpiration)
-
-	mux.Handle("/", http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-		// get id
-		id := strings.Trim(r.URL.Path, "/")
-		if id == "" {
-			w.WriteHeader(http.StatusBadRequest)
-			return
-		}
-
-		// prepare coll
-		var collection *madek.Collection
-
-		// read from cache if allowed
-		if cacheEnabled && r.URL.Query().Get("fresh") != "yes" {
-			val, ok := requestCache.Get(id)
-			if ok {
-				collection = val.(*madek.Collection)
-			}
-		}
-
-		// fetch not already there
-		if collection == nil {
-			coll, err := client.CompileCollection(id)
-			if err != nil {
-				w.WriteHeader(http.StatusInternalServerError)
-				return
-			}
-
-			// save in cache if allowed
-			if cacheEnabled {
-				requestCache.Set(id, coll, cache.NoExpiration)
-			}
-
-			collection = coll
-		}
-
-		// marshal
-		bytes, err := json.Marshal(collection)
-		if err != nil {
-			w.WriteHeader(http.StatusInternalServerError)
-			return
-		}
-
-		// write
-		w.WriteHeader(http.StatusOK)
-		_, _ = w.Write(bytes)
-	}))
-
-	fmt.Println("+------------------------------------------------------------+")
-	fmt.Println("| Running server on http://0.0.0.0:8080...                   |")
-	fmt.Println("| Load data using the following pattern:                     |")
-	fmt.Println("| > http://0.0.0.0:8080/82108639-c4a6-412d-b347-341fe5284caa |")
-	fmt.Println("+------------------------------------------------------------+")
-
-	_ = http.ListenAndServe("0.0.0.0:8080", mux)
 }
